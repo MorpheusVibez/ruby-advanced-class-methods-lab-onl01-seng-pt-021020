fe61199cf9bc01e87e227fed2026ec5d22f1e6c4
Andre Skeene <halodre24@gmail.com>
2020-02-29 19:23:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,4 @@ class Song
     @@all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name
-    if !find_by_name(name)
-      create_by_name(name)
-    else
-      find_by_name(name)
-    end
-  end
 end
